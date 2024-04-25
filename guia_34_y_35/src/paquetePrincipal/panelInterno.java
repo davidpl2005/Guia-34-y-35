8fad7b1c9af9438180531f557b7a03a12256d0d8
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 19:28:29 -0500

    MDI, MDI_CONT

---
+++ guia_34_y_35/src/paquetePrincipal/panelInterno.java
@@ -0,0 +1,16 @@
+
+package paquetePrincipal;
+
+import javax.swing.JInternalFrame;
+
+/**
+ *
+ * @author isac pacheco
+ */
+class panelInterno {
+
+    static JInternalFrame[] getAllFrames() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
